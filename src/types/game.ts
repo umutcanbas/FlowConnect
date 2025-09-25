4a95fd5a2a08844281205feeec8c5075c5ff5d62
umutcanbas <umutbas57@gmail.com>
2025-09-25 21:34:29 +0300

    Add game type definitions for Dot, LevelData, GameState, and related interfaces

---
+++ src/types/game.ts
@@ -0,0 +1,65 @@
+// Game Types
+export interface Dot {
+  color: string;
+  start: [number, number];
+  end: [number, number];
+}
+
+export interface LevelData {
+  dots: Dot[];
+}
+
+export interface Levels {
+  [key: string]: LevelData;
+}
+
+export type GridCell = string | number;
+export type Grid = GridCell[][];
+
+export interface StartDot {
+  row: number;
+  col: number;
+  color: string;
+}
+
+export interface PathCollection {
+  [color: string]: [number, number][];
+}
+
+export interface GameState {
+  grid: Grid;
+  currentLevel: string;
+  levelData: LevelData;
+  isLevelComplete: boolean;
+  isDragging: boolean;
+  currentPath: [number, number][];
+  startDot: StartDot | null;
+  completedPaths: PathCollection;
+  incompletePaths: PathCollection;
+}
+
+export interface GameBoardProps {
+  grid: Grid;
+  paths: {
+    [color: string]: { row: number; col: number }[];
+  };
+  onGestureEvent: any;
+  onHandlerStateChange: any;
+  gridSize: number;
+  cellSize: number;
+}
+
+export interface LevelHeaderProps {
+  currentLevel: string;
+  levelIndex: number;
+  totalLevels: number;
+}
+
+export interface GameControlsProps {
+  onUndo: () => void;
+  canUndo: boolean;
+  onReset: () => void;
+  isLevelComplete: boolean;
+  onNextLevel: () => void;
+  isLastLevel: boolean;
+}
